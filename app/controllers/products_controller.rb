7068ec0d8f565ae3e889cdc44a250f0aeeaa7f18
Kyle Geske <stungeye@gmail.com>
2015-03-17 10:24:23 -0500

    Implemented the show, new and create actions/views.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -5,4 +5,32 @@ class ProductsController < ApplicationController
   end
   # The associated view app/views/products/index.html.erb is auto-loaded.
   # The view gets access to all the action's instance variables.
+  
+  def show # The show action
+    @product = Product.find(params[:id])
+  end
+  # The associated view app/views/products/show.html.erb is
+  # auto-loaded. This view gets access to all instance vars.
+  
+  def new
+    @product = Product.new
+  end
+  # auto-loads: app/views/products/new.html.erb
+  
+  def create
+    @product = Product.new(whitelisted_params)
+    
+    if (@product.save)
+      # redirect_to product_path(@product.id) # Redirect to the show action for this product
+      redirect_to @product
+    else
+      render text: @product.errors.inspect
+    end
+  end
+  
+  private
+  def whitelisted_params
+    params.require(:product).permit(:name, :description, :price)
+  end
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
-  get 'wootly_grins' => 'products#index'
-  get 'products' => 'products#index'
+  # A HTTP GET request to /products will run the index action of the
+  # products controller.
+  
+  # CREATE
+  get  'products/new' => 'products#new'    # Show a new product form.
+  post 'products'     => 'products#create' # Create the new product.
+  
+  # READ
+  get  'products'     => 'products#index'  # Show all products
+  get  'products/:id' => 'products#show', as: 'product'   # Show a member of products (id)
+  
+  # UPDATE
+  # DESTROY
 end
