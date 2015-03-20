d3ed3926e941aa8c3afe04477b2421f4578c01a8
Kyle Geske <stungeye@gmail.com>
2015-03-20 11:12:30 -0500

    Implemented the rest of my CRUD tasks on product.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :load_product_by_id, only: [:show, :update, :edit, :destroy]
+
   def index # The index action
     # Use the Product model to load all products.
     @products = Product.all
@@ -7,7 +9,6 @@ class ProductsController < ApplicationController
   # The view gets access to all the action's instance variables.
   
   def show # The show action
-    @product = Product.find(params[:id])
   end
   # The associated view app/views/products/show.html.erb is
   # auto-loaded. This view gets access to all instance vars.
@@ -24,8 +25,29 @@ class ProductsController < ApplicationController
       # redirect_to product_path(@product.id) # Redirect to the show action for this product
       redirect_to @product
     else
-      render text: @product.errors.inspect
+      render :new
     end
+  end
+  # No associated view. no: app/views/products/create.html.erb
+  # If the product saves we redirect to the show
+  # If the validations fail we render the view associated with the new action.
+  
+  def edit
+  end
+  
+  def update
+    if (@product.update_attributes(whitelisted_params))
+      redirect_to @product
+    else
+      render :edit
+    end
+  end
+  # There is no app/views/products/update.html.erb
+  # We either redirect on success or render the edit view on failure.
+  
+  def destroy
+    @product.destroy
+    redirect_to root_path
   end
   
   private
@@ -33,4 +55,19 @@ class ProductsController < ApplicationController
     params.require(:product).permit(:name, :description, :price)
   end
   
+  def load_product_by_id
+    @product = Product.find(params[:id])
+  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,26 @@ Rails.application.routes.draw do
   # A HTTP GET request to /products will run the index action of the
   # products controller.
   
+  # HTTP Verbs
+  # GET
+  # POST
+  # PATCH / PUT
+  # DELETE
+  
+  root to: 'products#index'
+  
   # CREATE
-  get  'products/new' => 'products#new'    # Show a new product form.
+  get  'products/new' => 'products#new', as: 'new_product' # Show a new product form.
   post 'products'     => 'products#create' # Create the new product.
   
   # READ
-  get  'products'     => 'products#index'  # Show all products
-  get  'products/:id' => 'products#show', as: 'product'   # Show a member of products (id)
+  get  'products'     => 'products#index' # Show all products
+  get  'products/:id' => 'products#show', as: 'product', id: /\d+/ # Show a member of products (id)
   
   # UPDATE
+  get 'products/:id/edit' => 'products#edit', as: 'edit_product', id: /\d+/
+  patch 'products/:id' => 'products#update', id: /\d+/
+  
   # DESTROY
+  delete 'products/:id' => 'products#destroy', id: /\d+/
 end
